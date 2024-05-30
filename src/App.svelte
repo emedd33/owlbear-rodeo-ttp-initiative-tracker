8063fc0b4a9e31331107660b52cdf191d5a1e40b
Eskild Mageli <eskild.emedd33@gmail.com>
2024-05-30 14:53:08 +0200

    initial start

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
+<script>
+  import OBR from "@owlbear-rodeo/sdk";
+
+  const onClick = () => {
+    console.log("Button clicked");
+    OBR.notification.show(`Button clicked`);
+  };
+</script>
+
 <h1 class="text-3xl font-bold underline bg-slate-500">Hello world!</h1>
+<button class="bg-slate-500 text-white px-4 py-2 rounded" on:click={onClick}
+  >Click me</button
+>
