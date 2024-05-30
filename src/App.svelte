beddf003098c0b0c1da60dd5fa8763da5520afce
Eskild Mageli <eskild.emedd33@gmail.com>
2024-05-30 23:13:21 +0200

    add initial version

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,30 @@
-<script>
+<script lang="ts">
   import OBR from "@owlbear-rodeo/sdk";
+  OBR.broadcast.onMessage("rodeo.owlbear.tracker.message", async (event) => {
+    // Use the data from the event
+    if (event.data === "open") OBR.action.open();
+    else if (event.data === "close") OBR.action.close();
+  });
 
-  const onClick = () => {
-    console.log("Button clicked");
-    OBR.notification.show(`Button clicked`);
+  const onClick = async (signal: string) => {
+    const players = await OBR.party.getPlayers();
+    if (players.length === 0) {
+      return;
+    }
+    const receiver = players[0];
+    if (receiver) {
+      console.log(receiver);
+      OBR.broadcast.sendMessage("rodeo.owlbear.tracker.message", signal);
+    }
   };
 </script>
 
 <h1 class="text-3xl font-bold underline bg-slate-500">Hello world!</h1>
-<button class="bg-slate-500 text-white px-4 py-2 rounded" on:click={onClick}
-  >Click me</button
+<button
+  class="bg-slate-500 text-white px-4 py-2 rounded"
+  on:click={() => onClick("open")}>Open</button
+>
+<button
+  class="bg-slate-500 text-white px-4 py-2 rounded"
+  on:click={() => onClick("close")}>Close</button
 >
